a60c8dc0959fcea27f4bbfd6e25fea7763fa275a
barleviatias <barlevi.atias.com>
2024-02-18 16:40:33 +0200

    adding router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './App.css'
-import Register from './Comps/Register/Register'
-import Login from './Comps/Login/Login'
-
+import Register from './Comps/Register/Register.jsx'
+import Login from './Comps/Login/Login.jsx'
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
   return (
-    <div>
-      <Login />
-      <Register />
-    </div>
+    <>
+      <div>
+        <Link to="/">Home</Link> |<Link to="/register">Register</Link> |
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
